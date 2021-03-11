ab9ae6fef167e2406f52b6dc672c2d01e00b66c2
isaiahhaley0 <isaiah.haley@ucdenver.edu>
2021-03-10 18:39:16 -0700

    updated script

---
+++ js/main.js
@@ -0,0 +1,24 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            keword: '',
+            result: null
+        }
+
+    },
+
+
+    methods: {
+        searchGoogleBooks() {
+
+            fetch('https://www.googleapis.com/books/v1/volumes?&q=' + txtSearch.value + "&startIndex=0&maxResults=20")
+                .then(response => response.json())
+                .then(json => {
+                    this.result = json})
+
+                }
+
+
+    }
+
+    });
